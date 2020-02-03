8b77019bb0d367291a52fc8ca76343d34efb184a
Bobbi Tarkany <bl.tarkany@gmail.com>
2020-02-02 17:30:29 -0800

    Edited spelling on app.js question object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -206,7 +206,7 @@ var questions = [{
     answers: ["Kevin McCalister", "Jake Ryan", "Tony Manero", "Travis Bickle"],
     correctAnswer: "Jake Ryan"
 }, {
-    question: "In 'An Affair to Remeber', where were Nickie Ferrante and Terry Mckay supposed to meet in six months?",
+    question: "In 'An Affair to Remember', where were Nickie Ferrante and Terry Mckay supposed to meet in six months?",
     answers: ["Central Park", "Statue of Liberty", "Coney Island", "Empire State Building"],
     correctAnswer: "Empire State Building"
 }, {
@@ -214,19 +214,19 @@ var questions = [{
     answers: ["Audrey Hepburn", "Doris Day", "Marilyn Monroe", "Grace Kelly"],
     correctAnswer: "Grace Kelly"
 }, {
-    question: "In what moive must John McClane defeat Hans Gruber in order to save Christmas?",
+    question: "In what movie must John McClane defeat Hans Gruber in order to save Christmas?",
     answers: ["White Christmas", "Black Christmas", "Die Hard", "Black Christmas"],
     correctAnswer: "Die Hard"
 }, {
     question: "What's the name of the arch enemy Deadpool is searching for?",
-    answers: ["Colossus", "Francis", "Warlord", "Weasal"],
+    answers: ["Colossus", "Francis", "Warlord", "Weasel"],
     correctAnswer: "Francis"
 }, {
     question: "Who was the first black actress to win an Academy Award for 'Best Actress'?",
     answers: ["Viola Davis", "Eartha Kitt", "Cicely Tyson", "Halle Berry"],
     correctAnswer: "Halle Berry"
 }, {
-    question: "What was the first full length animated moive?",
+    question: "What was the first full length animated movie?",
     answers: ["Snow White", "Fun and Fancy Free", "Gulliver's Travels", "Pinocchio"],
     correctAnswer: "Snow White"
 }, {
@@ -246,7 +246,7 @@ var questions = [{
     answers: ["Dean Keaton", "Michael McManus", "Todd Hockney", "Verbal Kint"],
     correctAnswer: "Verbal Kint"
 }, {
-    question: "What was the name of Will Smith's character in 'Indepence Day'?",
+    question: "What was the name of Will Smith's character in 'Independence Day'?",
     answers: ["Jimmy Wilder", "David Levinson", "Russel Casse", "Steven Hiller"],
     correctAnswer: "Steven Hiller"
 }, {
@@ -259,7 +259,7 @@ var questions = [{
     correctAnswer: "The Wizard of Oz"
 }, {
     question: "In Harry Potter the meaning of the word 'Muggle' is?",
-    answers: ["a witch/wiazrd who can transform into an animal", "wizard prison", "non magical people", "fairies"],
+    answers: ["a witch/wizard who can transform into an animal", "wizard prison", "non magical people", "fairies"],
     correctAnswer: "non magical people"
 }];
 

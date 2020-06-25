0379d59135f462445d1da851b9301641eaf26179
Bobbi Tarkany <bl.tarkany@gmail.com>
2020-06-24 21:01:56 -0700

    added questions object to new js page to unclog app.js.

---
+++ assets/javascript/questions.js
@@ -0,0 +1,64 @@
+// questions
+// object to iterate through for game quiz
+
+const questions = [{
+    question: "What was the name of Molly Ringwalds love interest in Sixteen Candles?",
+    answers: ["Kevin McCalister", "Jake Ryan", "Tony Manero", "Travis Bickle"],
+    correctAnswer: "Jake Ryan"
+}, {
+    question: "In 'An Affair to Remember', where were Nickie Ferrante and Terry Mckay supposed to meet in six months?",
+    answers: ["Central Park", "Statue of Liberty", "Coney Island", "Empire State Building"],
+    correctAnswer: "Empire State Building"
+}, {
+    question: "Which 1950's actress became the Princess of Monaco?",
+    answers: ["Audrey Hepburn", "Doris Day", "Marilyn Monroe", "Grace Kelly"],
+    correctAnswer: "Grace Kelly"
+}, {
+    question: "In what movie must John McClane defeat Hans Gruber in order to save Christmas?",
+    answers: ["White Christmas", "Black Christmas", "Die Hard", "Black Christmas"],
+    correctAnswer: "Die Hard"
+}, {
+    question: "What's the name of the arch enemy Deadpool is searching for?",
+    answers: ["Colossus", "Francis", "Warlord", "Weasel"],
+    correctAnswer: "Francis"
+}, {
+    question: "Who was the first black actress to win an Academy Award for 'Best Actress'?",
+    answers: ["Viola Davis", "Eartha Kitt", "Cicely Tyson", "Halle Berry"],
+    correctAnswer: "Halle Berry"
+}, {
+    question: "What was the first full length animated movie?",
+    answers: ["Snow White", "Fun and Fancy Free", "Gulliver's Travels", "Pinocchio"],
+    correctAnswer: "Snow White"
+}, {
+    question: "What was the name of John Wick's dog, that his wife gave to him?",
+    answers: ["Brutus", "Patch", "Princess", "Daisy"],
+    correctAnswer: "Daisy"
+}, {
+    question: "In Hitchcock's 'The Birds', what kind of bird was Melanie Daniels bringing to Mitch Brenner?",
+    answers: ["Cockatiels", "Parakeets", "Lovebirds", "Canaries"],
+    correctAnswer: "Lovebirds"
+}, {
+    question: "What song did Aretha Franklin sing in 'Blues Brother'?",
+    answers: ["Respect", "Think", "Natural Woman", "Chain of Fools"],
+    correctAnswer: "Think"
+}, {
+    question: "Who is Keyser Soze in the film 'The Usual Suspects'?",
+    answers: ["Dean Keaton", "Michael McManus", "Todd Hockney", "Verbal Kint"],
+    correctAnswer: "Verbal Kint"
+}, {
+    question: "What was the name of Will Smith's character in 'Independence Day'?",
+    answers: ["Jimmy Wilder", "David Levinson", "Russel Casse", "Steven Hiller"],
+    correctAnswer: "Steven Hiller"
+}, {
+    question: "What movie is this line from, 'There's no crying in baseball'?",
+    answers: ["Major League", "A League of Their Own", "Bull Durham", "Rookie of the Year"],
+    correctAnswer: "A League of Their Own"
+}, {
+    question: "Judy Garland starred as Dorothy Gale in what classic film?",
+    answers: ["The Harvey Girls", "Pillow Talk", "Gentlemen Prefer Blondes", "The Wizard of Oz"],
+    correctAnswer: "The Wizard of Oz"
+}, {
+    question: "In Harry Potter the meaning of the word 'Muggle' is?",
+    answers: ["a witch/wizard who can transform into an animal", "wizard prison", "non magical people", "fairies"],
+    correctAnswer: "non magical people"
+}];

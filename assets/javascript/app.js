11de138897d63db1a0560abdb92606b19ebb7f96
Bobbi Tarkany <bl.tarkany@gmail.com>
2020-10-03 14:19:43 -0700

    added modal for testing in html, commeted out old code to run app test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,16 +2,19 @@
 
 // var to store timer
 let timer;
-let timeContainer = document.getElementById('timer')
+let timeContainer = document.getElementById('timer');
+// element grab
+let welcome = document.getElementById('startBtn');
+let gameDiv = document.getElementById('game-div');
 
 // variable to store game counters and functions
 let game = {
     // game counters
-    time = questions.length * 15,
-    correct = 0,
-    incorrect = 0, 
-    unanswered = 0,
-    currentIndex = 0,
+    time: questions.length * 15,
+    correct: 0,
+    incorrect: 0, 
+    unanswered: 0,
+    currentIndex: 0,
 
     // function to create countdown
     countDown: function(){
@@ -45,7 +48,7 @@ let game = {
         // iterate through the object array
         current.choices.forEach(function(choice, i){
             let option = document.createElement('button');
-            option.classList.add('btn btn-lg choice');
+            option.setAttribute('class', 'btn btn-lg choice');
             option.setAttribute('value', choice);
             option.textContent = choice;
             option.onclick = game.checkAnswer;
@@ -62,7 +65,7 @@ let game = {
             // display correct tab
         } else {
             game.incorrect++;
-            game.time -= 5;
+            game.time -= 10;
             // display incorrect tab
         }
 
@@ -73,6 +76,16 @@ let game = {
             game.populate();
         }
     },
+    start: () => {
+        // hide the welcome element, display game div
+        welcome.classList.add('hide');
+        gameDiv.classList.remove('hide');
+        // start timer
+        timer = setInterval(game.countDown, 1000);
+        // populate questions
+        game.populate();
+
+    },
     restart: () =>{
         // reset the game counters
         game.time = questions.length * 15;
@@ -81,8 +94,9 @@ let game = {
         game.unanswered = 0;
         game.currentIndex = 0;
         // run the start quiz function
-        
-    }
+
+    },
+
 
 }
 
@@ -97,124 +111,131 @@ $(document).on("click", "#restart", function () {
 });
 
 // counters
-let game = {
-    correct: 0,
-    incorrect: 0,
-    unanswered: 0,
-    timer: 120,
-    // timer count down function
-    decrement: function () {
-        game.timer--;
-        $("#timer").html("<div id='timer'><h2> Time Remaining: " + game.timer + " seconds</h2></div>");
-        if (game.timer === 0) {
-            clearInterval(countDown);
-            console.log("game over");
-            game.done();
-        }
-    },
-    // answer checks
-    done: function () {
-        clearInterval(countDown);
+// let game = {
+//     correct: 0,
+//     incorrect: 0,
+//     unanswered: 0,
+//     timer: 120,
+//     // timer count down function
+//     decrement: function () {
+//         game.timer--;
+//         $("#timer").html("<div id='timer'><h2> Time Remaining: " + game.timer + " seconds</h2></div>");
+//         if (game.timer === 0) {
+//             clearInterval(countDown);
+//             console.log("game over");
+//             game.done();
+//         }
+//     },
+//     // answer checks
+//     done: function () {
+//         clearInterval(countDown);
 
-        // condensed code for checking answers
-        for (var i = 0; i < questions.length; i++){
-            $.each($("input[name='question-"+[i]+"']:checked"), function() {
-                if ($(this).val() == questions[i].correctAnswer) {
-                    game.correct++;
-                } else {
-                    game.incorrect++;
-                }
-            });
-        }
+//         // condensed code for checking answers
+//         for (var i = 0; i < questions.length; i++){
+//             $.each($("input[name='question-"+[i]+"']:checked"), function() {
+//                 if ($(this).val() == questions[i].correctAnswer) {
+//                     game.correct++;
+//                 } else {
+//                     game.incorrect++;
+//                 }
+//             });
+//         }
 
         
-        console.log(game.incorrect + " incorrect");
-        console.log(game.correct + " correct");
-        game.results();
-    },
-    // display results and final game score
-    results: function () {
-        $("#timer").remove();
-        $("#gameBody").html("<h2>Let's see how you did!</h2>");
-        $("#gameBody").append("<img src='assets/images/clap.gif' alt='image' height='208' width='500'>");
-        $("#gameBody").append("<h3>Correct Answers: " + this.correct + "</h3>");
-        $("#gameBody").append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
-        $("#gameBody").append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
-        if (game.correct === 15) {
-            $("#gameBody").append("<h2>Perfect Score! Fantastic! You really know your movies!</h2>");
-            $("#gameBody").append("<button type='button' class='btn btn-info btn-lg' id='restart'>Start</button>");
+//         console.log(game.incorrect + " incorrect");
+//         console.log(game.correct + " correct");
+//         game.results();
+//     },
+//     // display results and final game score
+//     results: function () {
+//         $("#timer").remove();
+//         $("#gameBody").html("<h2>Let's see how you did!</h2>");
+//         $("#gameBody").append("<img src='assets/images/clap.gif' alt='image' height='208' width='500'>");
+//         $("#gameBody").append("<h3>Correct Answers: " + this.correct + "</h3>");
+//         $("#gameBody").append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+//         $("#gameBody").append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+//         if (game.correct === 15) {
+//             $("#gameBody").append("<h2>Perfect Score! Fantastic! You really know your movies!</h2>");
+//             $("#gameBody").append("<button type='button' class='btn btn-info btn-lg' id='restart'>Start</button>");
 
-        } else if (game.correct > game.incorrect) {
-            $("#gameBody").append("<h3>Not Bad! Want to try again?</h3>");
-            $("#gameBody").append("<button type='button' class='btn btn-info btn-lg' id='restart'>Start</button>");
+//         } else if (game.correct > game.incorrect) {
+//             $("#gameBody").append("<h3>Not Bad! Want to try again?</h3>");
+//             $("#gameBody").append("<button type='button' class='btn btn-info btn-lg' id='restart'>Start</button>");
 
-        } else {
-            $("#gameBody").append("<h3>Hmmmm..... Want to try again?</h3>");
-            $("#gameBody").append("<button type='button' class='btn btn-info btn-lg' id='restart'>Start</button>");
-        }
-    },
-    // restart game
-    restart: function () {
-        game.correct = 0;
-        game.incorrect = 0;
-        game.unanswered = 0;
-        game.timer = 120;
-        // start timer
-        countDown = setInterval(game.decrement, 1000);
-        // create div to append into so that it only appears when the button is clicked, includes timer div
-        $("#game").html("<div id='gameBody'><div id='timer'></div>");
-        // remove start button
-        $("#start").remove();
-        console.log("You clicked start");
-        console.log(questions);
-        // loop through questions with radio answers-break between questions and answers
-        for (var i = 0; i < questions.length; i++) {
-            $("#gameBody").append('<h3>' + questions[i].question + '<h3>');
-            for (var j = 0; j < questions[i].answers.length; j++) {
-                $("#gameBody").append("<input type='radio' name='question-" + i + "' value='" + questions[i].answers[j] + "'>" + questions[i].answers[j] + "<br>");
-            }
-            //    break between each question
-            $("#gameBody").append("<br>");
-        }
-        $("#gameBody").append("<button type='button' class='btn btn-info btn-lg' id='end'>Done</button>");
-    }
-}
+//         } else {
+//             $("#gameBody").append("<h3>Hmmmm..... Want to try again?</h3>");
+//             $("#gameBody").append("<button type='button' class='btn btn-info btn-lg' id='restart'>Start</button>");
+//         }
+//     },
+//     // restart game
+//     restart: function () {
+//         game.correct = 0;
+//         game.incorrect = 0;
+//         game.unanswered = 0;
+//         game.timer = 120;
+//         // start timer
+//         countDown = setInterval(game.decrement, 1000);
+//         // create div to append into so that it only appears when the button is clicked, includes timer div
+//         $("#game").html("<div id='gameBody'><div id='timer'></div>");
+//         // remove start button
+//         $("#start").remove();
+//         console.log("You clicked start");
+//         console.log(questions);
+//         // loop through questions with radio answers-break between questions and answers
+//         for (var i = 0; i < questions.length; i++) {
+//             $("#gameBody").append('<h3>' + questions[i].question + '<h3>');
+//             for (var j = 0; j < questions[i].answers.length; j++) {
+//                 $("#gameBody").append("<input type='radio' name='question-" + i + "' value='" + questions[i].answers[j] + "'>" + questions[i].answers[j] + "<br>");
+//             }
+//             //    break between each question
+//             $("#gameBody").append("<br>");
+//         }
+//         $("#gameBody").append("<button type='button' class='btn btn-info btn-lg' id='end'>Done</button>");
+//     }
+// }
 
 // variable for the interval counter
-let countDown;
+// let countDown;
 
 
 // timers
 // setInterval(game.decrement, 1000);
 
 // global functions
-function startgame() {
-    $("#start").on("click", function () {
-        // start timer
-        countDown = setInterval(game.decrement, 1000);
-        // create div to append into so that it only appears when the button is clicked, includes timer div
-        $("#game").append("<div id='gameBody'><div id='timer'></div>");
-        // remove start button
-        $("#start").remove();
-        console.log("You clicked start");
-        console.log(questions);
-        // loop through questions with radio answers-break between questions and answers
-        for (var i = 0; i < questions.length; i++) {
-            $("#gameBody").append('<h3>' + questions[i].question + '<h3>');
-            for (var j = 0; j < questions[i].answers.length; j++) {
-                $("#gameBody").append("<input type='radio' name='question-" + i + "' value='" + questions[i].answers[j] + "'>" + questions[i].answers[j] + "<br>");
-            }
-            //    break between each question
-            $("#gameBody").append("<br>");
-        }
-        $("#gameBody").append("<button type='button' class='btn btn-info btn-lg' id='end'>Done</button>");
-    })
-}
+// function startgame() {
+//     $("#start").on("click", function () {
+//         // start timer
+//         countDown = setInterval(game.decrement, 1000);
+//         // create div to append into so that it only appears when the button is clicked, includes timer div
+//         $("#game").append("<div id='gameBody'><div id='timer'></div>");
+//         // remove start button
+//         $("#start").remove();
+//         console.log("You clicked start");
+//         console.log(questions);
+//         // loop through questions with radio answers-break between questions and answers
+//         for (var i = 0; i < questions.length; i++) {
+//             $("#gameBody").append('<h3>' + questions[i].question + '<h3>');
+//             for (var j = 0; j < questions[i].answers.length; j++) {
+//                 $("#gameBody").append("<input type='radio' name='question-" + i + "' value='" + questions[i].answers[j] + "'>" + questions[i].answers[j] + "<br>");
+//             }
+//             //    break between each question
+//             $("#gameBody").append("<br>");
+//         }
+//         $("#gameBody").append("<button type='button' class='btn btn-info btn-lg' id='end'>Done</button>");
+//     })
+// }
 
 
 // Main Game Logic
 // ======================================================================
 $(document).ready(function () {
     // event.preventDefault();
-    startgame();
-})
+    $('#start').on('click', function(){
+        game.start();
+    })
+   
+
+    // $('#staticBackdrop').on('shown.bs.modal', function () {
+    //     $('#staticBackdrop').trigger('focus')
+    // });
+});
